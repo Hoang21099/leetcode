5b9f9f98a479bcd138f27e0f0c0ac7798875fa2e
Hoàng <hoang21099@gmail.com>
2024-05-10 21:12:37 +0700

    add solution dynamic programming to solve coin change problem

--- dp/coinChange.js
+++ dp/coinChange.js
@@ -1,104 +1,62 @@
-// A Naive recursive Javascript program to
-// find minimum of coins to make a given
-// change V
+/**322. Coin Change
+Solved
+Medium
+Topics
+Companies
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
-// m is size of coins array
-// (number of different coins)
-function minCoins(coins, m, V) {
-  // Base case
-  if (V == 0) return 0;
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
-  // Initialize result
-  let res = Number.MAX_VALUE;
+You may assume that you have an infinite number of each kind of coin.
 
-  // Try every coin that has smaller
-  // value than V
-  for (let i = 0; i < m; i++) {
-    if (coins[i] <= V) {
-      let sub_res = minCoins(coins, m, V - coins[i]);
+ 
 
-      // Check for INT_MAX to avoid overflow and
-      // see if result can minimized
-      if (sub_res != Number.MAX_VALUE && sub_res + 1 < res) res = sub_res + 1;
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0 */
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    for (let coin of coins) {
+      if (coin <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
     }
   }
-  return res;
-}
 
-// Driver code
-let coins = [2,3];
-let m = coins.length;
-let V = 5;
+  return dp[amount] > amount ? -1 : dp[amount];
+};
 
-console.log(minCoins(coins, m, V));
+/**Initialization of DP Array: This step takes O(amount) time, where amount is the target amount of money. We initialize an array of size amount + 1 and fill it with initial values, which takes linear time.
+Updating DP Array: We have two nested loops:
+The outer loop runs from 1 to amount, so it iterates amount times.
+The inner loop iterates through each coin, but the number of iterations for the inner loop depends on the number of coins available. Let's denote the number of coins as n. Therefore, the inner loop can take at most O(n) time.
+The overall time complexity of the solution is O(amount * n), where:
 
+amount is the target amount of money.
+n is the number of coins.
+In the worst case, if amount is large and n is also large (many coins), the time complexity can be significant.
 
+The space complexity of the solution is O(amount) because we use a DP array of size amount + 1 to store the minimum number of coins needed for each amount.
+ */
 
-
-// Quick Note - Greedy Programming v/s Dynamic Programming
-// Coin Change Problem: DP and Recursion Approach
-// A Definitive Guide to Knapsack Problem
-// How to Solve Subset Sum Problem?
-// Understanding Huffman Coding in detail
-// Understand the working of KMP Algorithm
-// Longest Common Substring Problem
-// Longest Common Subsequence problem: solved
-// A Quick Guide to Manacher's Algorithm
-// Learning About Bipartite Graphs
-// Graph Coloring Problem: Explained
-// Detect Cycle in Direct Graph
-// Directed Acyclic Graph: Representation
-// Prim's Algorithm: Explanation, Code, and Applications
-// Working of Kruskal's Algorithm
-// Prims and Kruskal algorithm for Maximum Spanning Tree
-// Bellman Ford Algorithm in detail with code
-// Floyd-Warshall Algorithm and its Implementation
-// Understand Travelling Salesman Problem
-// Branch And Bound Algorithm: Explained
-// How to Evaluate Postfix Expression
-// Introduction to Round-Robin Scheduling
-// Disjoint set (Union find Algorithm)
-// State Space Reduction in Algorithms
-// Apriori Algorithm
-// What is A* Search Algorithm?
-// Outline of Article
-// Naive approach - Brute Force Recursion
-// Pro Approach - Dynamic Programming & Recursion [Optimal Time Complexity]
-// All codes are in C++, Java, and Python.
-
-// Introduction
-// Problem: Given a set of coins and a value 'V'. Find the number of ways
-
-// we can make a change of 'V'.  
-
-// OR
-
-// You are given an array of coins of size n, each representing a coin of a distinct denomination. Target is another value that you are given. Discover the various combinations that make up the desired quantity. Consider that there are endless quantities of each type of coin.
-
-// Ex : Problem: S = {1,2,3} V = 3
-
-// Possible ways to make change are; {3}, {2,1}, {1,1,1}
-
-// Note: {1,2} are not counted as separate as it is the same as {2,1}
-
-// Naive Approach - Exponential Complexity
-// We can use a brute force recursion to fix this issue crudely. We can try every conceivable combination of taking coins to equal the desired amount, adding them all up to determine how many ways there are to get the desired amount.
-// The simplest scenario would be when we have no coins or when the goal is to reach zero.
-// Time Complexity: O(targetn) // Exponential
-// Space Complexity: O(targetn) // Exponential
-// int coinChange(vector<int> &coins, int n, int target) {
-//    if(target == 0) {
-//        return 1;
-//    }
-//    if(target < 0) {
-//        return 0;
-//    }
-//    if(n <= 0  && target > 0) {
-//        return 0;
-//    }
-//    return coinChange(coins, n - 1, target) + coinChange(coins, n, target - coins[n - 1]);
-// }
-// int numberOfCombinations(vector<int> &coins, int target) {
-//    int n = coins.size();
-//    return coinChange(coins, n, target);
-// }
+console.log(coinChange([1, 2, 5], 11)); // 3
+console.log(coinChange([2], 3)); // -1
