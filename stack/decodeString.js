1f5c09b447f64358db83f5645975ef06003fffbb
Hoàng <hoang21099@gmail.com>
2024-05-11 11:52:10 +0700

    add solution to resove 394. Decode String using stack

---
+++ stack/decodeString.js
@@ -0,0 +1,66 @@
+/**394. Decode String
+ * Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char !== "]") {
+      stack.push(char);
+    } else {
+      let tempString = "";
+      let count = "";
+      while (stack[stack.length - 1] !== "[") {
+        tempString = stack.pop() + tempString;
+      }
+
+      if (stack[stack.length - 1] === "[") stack.pop();
+
+      while (!isNaN(stack[stack.length - 1])) {
+        count = stack.pop() + count;
+      }
+
+      let decodedString = "";
+
+      for (let j = 0; j < +count; j++) {
+        decodedString += tempString;
+      }
+
+      stack.push(decodedString);
+    }
+  }
+  return stack.join("");
+};
+// Time complexity is O(n) where n is the length of the input string s.
+// Space complexity is O(n) where n is the length of the input string s.
+console.log(decodeString("3[a]2[bc]")); // "aaabcbc"
+console.log(decodeString("3[a2[c]]")); // "accaccacc"
+console.log(decodeString("2[abc]3[cd]ef")); // "abcabccdcdcdef"
